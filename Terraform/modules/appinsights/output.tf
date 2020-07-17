3ac676cf6b27c63acc290ecdeecc00ef23d1db0c
Prem Londhe <premlondhe@yahoo.com>
2020-07-17 16:58:30 -0500

    Updated output.tf

--- Terraform/modules/appinsights/output.tf
+++ Terraform/modules/appinsights/output.tf
@@ -1,8 +1,8 @@
 output "app_id" {
-  value = azurerm_application_insights.example.app_id
+  value = azurerm_application_insights.main.app_id
 }
 
 output "instrumentation_key" {
-  value = azurerm_application_insights.example.instrumentation_key
+  value = azurerm_application_insights.main.instrumentation_key
 }
 
